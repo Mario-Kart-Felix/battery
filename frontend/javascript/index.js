36ccc57dc2b24f3969450c2f0a7b1a4c7210e0f9
Chris Wade <chris@wadespace.net>
2021-02-10 15:51:58 +1100

    setup

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,37 +1,43 @@
 import "index.scss"
 import 'bootstrap';
-
 // Import all javascript files from src/_components
 const componentsContext = require.context("bridgetownComponents", true, /.js$/)
 componentsContext.keys().forEach(componentsContext)
 
-console.info("lets pasdfasdasdfasdfasfasdfsafddfarty jh!")
+console.info("lets  jh!")
 // Burger menus
-document.addEventListener('DOMContentLoaded', function () {
-  // open/close
-  const toggler = document.querySelectorAll('[data-toggle="side-menu"]');
+document.addEventListener('DOMContentLoaded', function() {
+    // open/close
+    const toggler = document.querySelectorAll('[data-toggle="side-menu"]');
 
-  if (toggler.length) {
-    for (var i = 0; i < toggler.length; i++) {
-      const target = toggler[i].getAttribute('data-target');
+    if (toggler.length) {
+        for (var i = 0; i < toggler.length; i++) {
+            const target = toggler[i].getAttribute('data-target');
 
-      if (target.length) {
-        toggler[i].addEventListener('click', function (event) {
-          event.preventDefault();
-          const menu = document.querySelector(target);
+            if (target.length) {
+                toggler[i].addEventListener('click', function(event) {
+                    event.preventDefault();
+                    const menu = document.querySelector(target);
 
-          if (menu) {
-            menu.classList.toggle('d-none');
-          }
-        });
+                    if (menu) {
+                        menu.classList.toggle('d-none');
+                    }
+                });
             }
+        }
     }
-  }
 });
+console.info("YAY for coding !!")
+const batt = document.getElementById('batt');
+const battId = document.getElementById('bat-id');
 
-const battery = document.querySelector('#batasdfasfdt');
 
-
-
-console.log("Brdasdfasdfasded!")
-
+var i = 0;
+batt.addEventListener('click', (event) => {
+    // Callback
+    var i = battId.value;
+    console.log(i);
+    battId.value = ++i;
+    console.log(`i clicked ${i} times`)
+    // event.currentTarget.setAttribute("disabled", "");
+});
